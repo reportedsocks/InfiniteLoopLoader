c22a7516eed7ea283317f097966e87ea022357c7
daniil Antsyferov <dreeantsyferov@gmail.com>
2022-06-19 15:13:20 +0200

    added scaling animation, styling parameters, code cleanup

--- app/src/main/java/com/example/looploader/MainActivity.kt
+++ app/src/main/java/com/example/looploader/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.looploader
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.lib.LoopLoader
@@ -8,8 +9,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val loopLoader = findViewById<LoopLoader>(R.id.loop_loader)
-        loopLoader.startAnimation()
-        loopLoader.numberOfSegments = 5
+
+        val loopLoader1 = findViewById<LoopLoader>(R.id.loop_loader1)
+        loopLoader1.startAnimation()
+        loopLoader1.numberOfSegments = 5 // wont work
+        loopLoader1.rotationDirection = LoopLoader.RotationDirection.COUNTERCLOCKWISE
+        loopLoader1.segmentPaintWidth = 20f
+        loopLoader1.segmentRotationDuration = 500
+        loopLoader1.setBackgroundColor(Color.BLACK)
+
+
+        val loopLoader2 = findViewById<LoopLoader>(R.id.loop_loader2)
+        loopLoader2.startAnimation()
+
+        val loopLoader3 = findViewById<LoopLoader>(R.id.loop_loader3)
+        loopLoader3.startAnimation()
     }
 }
--- lib/src/main/java/com/example/lib/LoopLoader.kt
+++ lib/src/main/java/com/example/lib/LoopLoader.kt
@@ -4,33 +4,48 @@ import android.animation.Animator
 import android.animation.AnimatorSet
 import android.animation.ValueAnimator
 import android.content.Context
-import android.graphics.*
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.RectF
 import android.util.AttributeSet
 import android.view.View
 import android.view.animation.AccelerateInterpolator
-import androidx.core.content.res.ResourcesCompat
+import androidx.core.graphics.withScale
 import kotlin.math.min
 
 class LoopLoader: View {
 
-    constructor(context: Context) : super(context)
-    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
-    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs, defStyle)
-
     companion object {
         private const val CIRCLE_TOP = 270f
         private const val TOTAL_DEGREES = 360f
+        private const val DEFAULT_SCALE = 1f
     }
 
-    private lateinit var extraCanvas: Canvas
-    private lateinit var extraBitmap: Bitmap
+    var segmentPaintWidth = 100f
+        set(value) {
+            evenSegmentPaint.strokeWidth = value
+            oddSegmentPaint.strokeWidth = value
+            field = value
+        }
+    var segmentRotationDuration = 700L
+    var segmentTransformationDuration = 200L
+    var activeSegmentColor = context?.resources?.getColor(R.color.white, context.theme) ?: Color.GRAY
+    var passiveSegmentColor = context?.resources?.getColor(R.color.light_gray, context.theme) ?: Color.GRAY
+    var numberOfSegments = 6
+        set(value) {
+            if (value % 2 != 0) return  // check for even number
+            segmentWidth = TOTAL_DEGREES / value
+            field = value
+        }
+    var maxScale = 1.05f
+        set(value) {
+            maxScaleOverhead = value - DEFAULT_SCALE
+            field = value
+        }
+    var rotationDirection = RotationDirection.CLOCKWISE
 
     private val rect = RectF()
-
-    private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, context.theme)
-
-    private val activeSegmentColor = context?.resources?.getColor(R.color.white, context.theme) ?: Color.GRAY
-    private val passiveSegmentColor = context?.resources?.getColor(R.color.light_gray, context.theme) ?: Color.GRAY
 
     private var actualActiveColor = activeSegmentColor
     private var actualPassiveColor = activeSegmentColor
@@ -38,21 +53,14 @@ class LoopLoader: View {
     private val evenSegmentPaint = Paint().apply {
         style = Paint.Style.STROKE
         isAntiAlias = true
-        strokeWidth = 100f
+        strokeWidth = segmentPaintWidth
     }
-
     private val oddSegmentPaint = Paint().apply {
         style = Paint.Style.STROKE
         isAntiAlias = true
-        strokeWidth = 100f
+        strokeWidth = segmentPaintWidth
     }
 
-    var numberOfSegments = 6
-        set(value) { // check for even number, then update all involved constants
-            if (value % 2 != 0) return
-            segmentWidth = TOTAL_DEGREES / value
-            field = value
-        }
     private var segmentWidth = TOTAL_DEGREES / numberOfSegments
         set(value) {
             oddOffset = CIRCLE_TOP + value
@@ -66,51 +74,111 @@ class LoopLoader: View {
 
     private var isAnimatingEvenSegments = true
 
-    override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
-        super.onSizeChanged(width, height, oldWidth, oldHeight)
+    private var horizontalCenter = 0f
+    private var verticalCenter = 0f
+    private var rectSize = 0f
 
-        if (::extraBitmap.isInitialized) extraBitmap.recycle()
-        extraBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+    private var maxScaleOverhead = maxScale - DEFAULT_SCALE
+        set(value) {
+            minScaleOverhead = maxScale - DEFAULT_SCALE - value
+            field = value
+        }
+    private var minScaleOverhead = maxScale - DEFAULT_SCALE - maxScaleOverhead
 
-        extraCanvas = Canvas(extraBitmap)
-        extraCanvas.drawColor(backgroundColor)
-        calculateRect()
+    private var animationEndWasCalled = false
+
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        val a = context.obtainStyledAttributes(attrs, R.styleable.LoopLoader)
+
+        if (a.hasValue(R.styleable.LoopLoader_segmentPaintWidth))
+            segmentPaintWidth = a.getFloat(R.styleable.LoopLoader_segmentPaintWidth, segmentPaintWidth)
+        if (a.hasValue(R.styleable.LoopLoader_segmentRotationDuration))
+            segmentRotationDuration = a.getInt(R.styleable.LoopLoader_segmentRotationDuration, segmentRotationDuration.toInt()).toLong()
+        if (a.hasValue(R.styleable.LoopLoader_segmentTransformationDuration))
+            segmentTransformationDuration = a.getInt(R.styleable.LoopLoader_segmentTransformationDuration, segmentTransformationDuration.toInt()).toLong()
+        if (a.hasValue(R.styleable.LoopLoader_activeSegmentColor))
+            activeSegmentColor = a.getColor(R.styleable.LoopLoader_activeSegmentColor, activeSegmentColor)
+        if (a.hasValue(R.styleable.LoopLoader_passiveSegmentColor))
+            passiveSegmentColor = a.getColor(R.styleable.LoopLoader_passiveSegmentColor, passiveSegmentColor)
+        if (a.hasValue(R.styleable.LoopLoader_numberOfSegments))
+            numberOfSegments = a.getInt(R.styleable.LoopLoader_numberOfSegments, numberOfSegments)
+        if (a.hasValue(R.styleable.LoopLoader_maxScale))
+            maxScale = a.getFloat(R.styleable.LoopLoader_maxScale, maxScale)
+        if (a.hasValue(R.styleable.LoopLoader_rotationDirection))
+            rotationDirection =
+                RotationDirection.values()[a.getInt(R.styleable.LoopLoader_rotationDirection, 0)]
+        a.recycle()
+    }
+    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs, defStyle)
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        horizontalCenter = (width.div(2)).toFloat()
+        verticalCenter = (height.div(2)).toFloat()
+        rectSize = min(width,height) / 3f
     }
 
-    private fun calculateRect() {
-        val horizontalCenter = (width.div(2)).toFloat()
-        val verticalCenter = (height.div(2)).toFloat()
-        val rectSize = min(width,height) / 4
-        rect.set(
-            horizontalCenter - rectSize,
-            verticalCenter - rectSize,
-            horizontalCenter + rectSize,
-            verticalCenter + rectSize
-        )
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        val size = min(measuredHeight, measuredWidth)
+        setMeasuredDimension(size, size)
     }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        canvas.drawBitmap(extraBitmap, 0f, 0f, null)
+
+        evenSegmentPaint.color = if (isAnimatingEvenSegments) actualActiveColor else actualPassiveColor
+        oddSegmentPaint.color = if (isAnimatingEvenSegments) actualPassiveColor else actualActiveColor
+
+        scaleAndDraw(
+            canvas,
+            if (isAnimatingEvenSegments) { {drawOddSegments(canvas, oddOffset)} } else { {drawEvenSegments(canvas, evenOffset)} },
+            if (isAnimatingEvenSegments) { {drawEvenSegments(canvas, getEvenActiveOffset())} } else { {drawOddSegments(canvas, getOddActiveOffset())} }
+        )
+
+    }
+
+    private fun getEvenActiveOffset() =
+        if (rotationDirection == RotationDirection.CLOCKWISE)
+            evenOffset + offsetRotationModifier else evenOffset - offsetRotationModifier
+
+    private fun getOddActiveOffset() =
+        if (rotationDirection == RotationDirection.CLOCKWISE)
+            oddOffset + offsetRotationModifier else oddOffset - offsetRotationModifier
 
 
-        if (isAnimatingEvenSegments) {
+    private fun scaleAndDraw(canvas: Canvas, passiveSegments: () -> Unit, activeSegments: () -> Unit) {
 
-            evenSegmentPaint.color = actualActiveColor
-            oddSegmentPaint.color = actualPassiveColor
+        val downScale = DEFAULT_SCALE + minScaleOverhead
+        val upScale = DEFAULT_SCALE + maxScaleOverhead
 
-            //change order of drawing so rotating segments are on top
-            drawOddSegments(canvas, oddOffset)
-            drawEvenSegments(canvas, evenOffset + offsetRotationModifier)
-        } else {
-
-            evenSegmentPaint.color = actualPassiveColor
-            oddSegmentPaint.color = actualActiveColor
-
-            drawEvenSegments(canvas, evenOffset)
-            drawOddSegments(canvas, oddOffset + offsetRotationModifier)
+        canvas.withScale(
+            downScale,
+            downScale,
+            horizontalCenter,
+            verticalCenter) {
+            calculateRect(downScale)
+            passiveSegments()
         }
+        canvas.withScale(
+            upScale,
+            upScale,
+            horizontalCenter,
+            verticalCenter) {
+            calculateRect(upScale)
+            activeSegments()
+        }
+    }
 
+    private fun calculateRect(scale: Float = DEFAULT_SCALE) {
+        val scaledRectSize = rectSize / scale
+        rect.set(
+            horizontalCenter - scaledRectSize,
+            verticalCenter - scaledRectSize,
+            horizontalCenter + scaledRectSize,
+            verticalCenter + scaledRectSize
+        )
     }
 
     private fun drawEvenSegments(canvas: Canvas,  offset: Float) {
@@ -130,46 +198,24 @@ class LoopLoader: View {
     }
 
     fun startAnimation() {
-
-        val rotationAnimator = ValueAnimator.ofFloat(0f, segmentWidth * 2).apply {
-            duration = 700
-            interpolator = AccelerateInterpolator()
-            addUpdateListener {
-                offsetRotationModifier = it.animatedValue as Float
-                invalidate()
-            }
-        }
-
-        val passiveToActiveColorAnimator = ValueAnimator.ofArgb(passiveSegmentColor, activeSegmentColor).apply {
-            duration = 200
-            addUpdateListener {
-                actualActiveColor = it.animatedValue as Int
-                invalidate()
-            }
-        }
-
-        val activeToPassiveColorAnimator = ValueAnimator.ofArgb(activeSegmentColor, passiveSegmentColor).apply {
-            duration = 200
-            addUpdateListener {
-                actualPassiveColor = it.animatedValue as Int
-                invalidate()
-            }
-        }
-
-        val animatorSet = AnimatorSet().apply {
-            play(passiveToActiveColorAnimator).with(activeToPassiveColorAnimator)
-            play(rotationAnimator).after(passiveToActiveColorAnimator)
+        AnimatorSet().apply {
+            play(getPassiveToActiveColorAnimator())
+                .with(getActiveToPassiveColorAnimator())
+                .with(getScaleAnimator())
+                .before(getRotationAnimator())
 
             addListener(object: Animator.AnimatorListener {
                 override fun onAnimationStart(animator: Animator?) {}
-                override fun onAnimationEnd(animator: Animator?) {
-                    isAnimatingEvenSegments = !isAnimatingEvenSegments
-                    animator?.start()
-                }
                 override fun onAnimationCancel(animator: Animator?) {}
+                override fun onAnimationRepeat(animator: Animator?) {}
 
-                override fun onAnimationRepeat(animator: Animator?) {
-
+                override fun onAnimationEnd(animator: Animator?) {
+                    if (animationEndWasCalled) {
+                        animationEndWasCalled = false
+                    } else {
+                        isAnimatingEvenSegments = !isAnimatingEvenSegments
+                        animator?.start()
+                    }
                 }
             })
 
@@ -177,4 +223,51 @@ class LoopLoader: View {
         }
     }
 
+    private fun getRotationAnimator() = ValueAnimator.ofFloat(0f, segmentWidth * 2)
+        .apply {
+            duration = segmentRotationDuration
+            interpolator = AccelerateInterpolator()
+            addUpdateListener {
+                offsetRotationModifier = it.animatedValue as Float
+                invalidate()
+            }
+        }
+
+    private fun getPassiveToActiveColorAnimator() = ValueAnimator.ofArgb(passiveSegmentColor, activeSegmentColor)
+        .apply {
+            duration = segmentTransformationDuration
+            addUpdateListener {
+                actualActiveColor = it.animatedValue as Int
+                invalidate()
+            }
+        }
+
+    private fun getActiveToPassiveColorAnimator() = ValueAnimator.ofArgb(activeSegmentColor, passiveSegmentColor)
+        .apply {
+            duration = segmentTransformationDuration
+            addUpdateListener {
+                actualPassiveColor = it.animatedValue as Int
+                invalidate()
+            }
+        }
+
+    private fun getScaleAnimator() = ValueAnimator.ofFloat(0f, maxScale - DEFAULT_SCALE )
+        .apply {
+            duration = segmentTransformationDuration
+            addUpdateListener {
+                maxScaleOverhead = it.animatedValue as Float
+                invalidate()
+            }
+        }
+
+
+    fun endAnimation() {
+        animationEndWasCalled = true
+    }
+
+    enum class RotationDirection {
+        CLOCKWISE,
+        COUNTERCLOCKWISE
+    }
 }
+
